809eedc3413a6aa19493a5a4170af83dc28a8fce
Joshua Quaintance <pelealu.2@wright.edu>
2024-10-17 12:09:57 -0400

    Bot Initialization:   - A simple ping command   - Imports all the commands and loads it into memory   - Updates the command in the discord REST API so that the end user can see the slash command   - Custom Commands

---
+++ src/apps/bot/src/util/loadCommands.ts
@@ -0,0 +1,41 @@
+import { readdir, stat } from 'fs/promises';
+import { join } from 'path';
+import { Collection, Client, RESTPostAPIChatInputApplicationCommandsJSONBody } from 'discord.js';
+import { ClientApp, CommandObject } from '../types';
+
+/**
+ * Loads all the commands putting it into the client
+ *
+ * @param { Client } client
+ * @returns { Promise< import('../typedefs.js').Command[] > }
+ */
+const loadCommands = async (client?: ClientApp) => {
+    let commands: Collection<string, CommandObject> = new Collection();
+
+    if (client) {
+        client.commands = new Collection();
+        commands = client.commands;
+    }
+
+    const commandsArr: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
+
+    const files = await readdir(join(process.cwd(), 'dist', 'commands'));
+
+    for (const file of files) {
+        const loc = join(process.cwd(), 'dist', 'commands', file);
+        const check = await stat(loc);
+
+        if (check.isDirectory()) continue;
+
+        const command: CommandObject = (await import(loc)).default;
+
+        if ('data' in command && 'execute' in command) {
+            if (client) commands.set(command.data.name, command);
+            commandsArr.push(command.data.toJSON());
+        }
+    }
+
+    return commandsArr;
+};
+
+export default loadCommands;
---
+++ src/apps/bot/src/util/updateCommands.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+import loadCommands from './loadCommands';
+
+async function updateCommands() {
+    try {
+        const rest = new REST().setToken(<string>process.env.BOT_TOKEN);
+        const CLIENT_ID = process.env.CLIENT_ID;
+        const commandsArr = await loadCommands();
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(Routes.applicationCommands(<string>CLIENT_ID), { body: commandsArr });
+
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error, 'Unexpected error during updating slash commands');
+    }
+}
+
+await updateCommands();
